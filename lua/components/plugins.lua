1d2d05e1755a570607989f793958668e749a5f24
orzorzzhang <orzorzzhang@tencent.com>
2024-09-09 18:38:12 +0800

    modify config

--- lua/components/plugins.lua
+++ lua/components/plugins.lua
@@ -1,168 +1,147 @@
 local M = {}
 
 M.list = {
-    -- 展示颜色
-    -- TODO: cmp 集成
-    {
-        "NvChad/nvim-colorizer.lua",
-        event = "VeryLazy",
-        config = function()
-            local r = try_require("colorizer")
-            if r == nil then
-                return
-            end
+	-- 展示颜色
+	-- TODO: cmp 集成
+	{
+		"NvChad/nvim-colorizer.lua",
+		event = "VeryLazy",
+		config = function()
+			local r = try_require("colorizer")
+			if r == nil then
+				return
+			end
 
-            r.setup({
-                filetypes = {
-                    "*", -- Highlight all files, but customize some others.
-                    cmp_docs = { always_update = true },
-                },
-                RGB = true, -- #RGB hex codes
-                RRGGBB = true, -- #RRGGBB hex codes
-                names = true, -- "Name" codes like Blue or blue
-                RRGGBBAA = true, -- #RRGGBBAA hex codes
-                AARRGGBB = true, -- 0xAARRGGBB hex codes
-                rgb_fn = true, -- CSS rgb() and rgba() functions
-                hsl_fn = true, -- CSS hsl() and hsla() functions
-                css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-                css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-            })
-        end,
-    },
+			r.setup({
+				filetypes = {
+					"*", -- Highlight all files, but customize some others.
+					cmp_docs = { always_update = true },
+				},
+				RGB = true, -- #RGB hex codes
+				RRGGBB = true, -- #RRGGBB hex codes
+				names = true, -- "Name" codes like Blue or blue
+				RRGGBBAA = true, -- #RRGGBBAA hex codes
+				AARRGGBB = true, -- 0xAARRGGBB hex codes
+				rgb_fn = true, -- CSS rgb() and rgba() functions
+				hsl_fn = true, -- CSS hsl() and hsla() functions
+				css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+				css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+			})
+		end,
+	},
 
-    --HACK:
-    --TODO:
-    --FIX:
-    --NOTE:
-    --WARNING:
-    {
-        "folke/todo-comments.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        opts = {},
-        -- cmd = { "Todo", "TodoQuickFix", "TodoTelescope", "TodoTrouble", "TodoLocList" },
-        config = function()
-            try_require("todo-comments").setup({})
+	--TODO:
+	--FIX:
+	--NOTE:
+	--WARN:
+	{
+		"echasnovski/mini.hipatterns",
+		version = false,
+		config = function()
+			require("components.todo").config()
+		end,
+	},
 
-            local telescope = try_require("telescope")
-            if telescope == nil then
-                return
-            end
-            telescope.setup({
-                extensions = {
-                    ["todo-comments"] = {},
-                },
-            })
-            telescope.load_extension("todo-comments")
+	-- Neovim 中人类可读的内联 cron 表达式
+	{
+		"fabridamicelli/cronex.nvim",
+		opts = {},
+		ft = { "go" },
+		config = function()
+			local r = try_require("components.cron")
+			if r ~= nil then
+				r.cronConfig()
+			end
+		end,
+	},
 
-            Del_cmd("TodoQuickFix")
-            Del_cmd("TodoTelescope")
-            Del_cmd("TodoTrouble")
-            Del_cmd("TodoLocList")
+	-- 翻译插件
+	{
+		cmd = { "Translate" },
+		"uga-rosa/translate.nvim",
+	},
 
-            cmd("command! -nargs=* Todo Telescope todo-comments todo <args>")
-        end,
-    },
+	-- markdown预览
+	{
+		"OXY2DEV/markview.nvim",
+		-- lazy = false, -- Recommended
+		ft = "markdown", -- If you decide to lazy-load anyway
 
-    -- Neovim 中人类可读的内联 cron 表达式
-    {
-        "fabridamicelli/cronex.nvim",
-        opts = {},
-        ft = { "go" },
-        config = function()
-            local r = try_require("components.cron")
-            if r ~= nil then
-                r.cronConfig()
-            end
-        end,
-    },
+		dependencies = {
+			-- You will not need this if you installed the
+			-- parsers manually
+			-- Or if the parsers are in your $RUNTIMEPATH
 
-    -- 翻译插件
-    {
-        cmd = { "Translate" },
-        "uga-rosa/translate.nvim",
-    },
+			"nvim-tree/nvim-web-devicons",
 
-    -- markdown预览
-    {
-        "OXY2DEV/markview.nvim",
-        -- lazy = false, -- Recommended
-        ft = "markdown", -- If you decide to lazy-load anyway
+			-- Otter.nvim 为其他文档中嵌入的代码提供 lsp 功能和代码补全源
+			{
+				"jmbuhr/otter.nvim",
+				ft = "markdown", -- If you decide to lazy-load anyway
+				dependencies = {
+					"nvim-treesitter/nvim-treesitter",
+				},
+				opts = {},
+			},
+		},
+		config = function()
+			require("markview").setup({})
+		end,
+	},
 
-        dependencies = {
-            -- You will not need this if you installed the
-            -- parsers manually
-            -- Or if the parsers are in your $RUNTIMEPATH
+	-- precognition.nvim - 预识别使用虚拟文本和装订线标志来显示可用的动作。
+	-- Precognition toggle
+	-- {
+	-- 	"tris203/precognition.nvim",
+	-- 	opts = {},
+	-- },
 
-            "nvim-tree/nvim-web-devicons",
+	-- Neovim 插件帮助您建立良好的命令工作流程并戒掉坏习惯
+	{
+		"m4xshen/hardtime.nvim",
+		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+		config = function()
+			require("hardtime").setup({
+				disable_mouse = false,
+				restricted_keys = {
+					["j"] = {},
+					["k"] = {},
+				},
+				disabled_keys = {
+					["<Up>"] = {},
+					["<Down>"] = {},
+					["<Left>"] = {},
+					["<Right>"] = {},
+				},
+			})
+		end,
+	},
 
-            -- Otter.nvim 为其他文档中嵌入的代码提供 lsp 功能和代码补全源
-            {
-                "jmbuhr/otter.nvim",
-                ft = "markdown", -- If you decide to lazy-load anyway
-                dependencies = {
-                    "nvim-treesitter/nvim-treesitter",
-                },
-                opts = {},
-            },
-        },
-        config = function()
-            require("markview").setup({})
-        end,
-    },
+	-- 使用 curl 运行请求，使用 jq 格式化，并根据您自己的工作流程保存命令
+	-- 这些命令将打开 curl.nvim 选项卡。在左侧缓冲区中，您可以粘贴或写入 curl 命令，然后按 Enter 键，命令将执行，并且输出将在最右侧的缓冲区中显示和格式化。
+	-- 如果您愿意，您可以选择右侧缓冲区中的文本，并使用 jq 对其进行过滤，即 ggVG! jq '{query goes here}'
+	{
+		"oysandvik94/curl.nvim",
+		cmd = { "CurlOpen" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = true,
+	},
 
-    -- precognition.nvim - 预识别使用虚拟文本和装订线标志来显示可用的动作。
-    -- Precognition toggle
-    -- {
-    -- 	"tris203/precognition.nvim",
-    -- 	opts = {},
-    -- },
-
-    -- Neovim 插件帮助您建立良好的命令工作流程并戒掉坏习惯
-    {
-        "m4xshen/hardtime.nvim",
-        dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-        config = function()
-            require("hardtime").setup({
-                disable_mouse = false,
-                restricted_keys = {
-                    ["j"] = {},
-                    ["k"] = {},
-                },
-                disabled_keys = {
-                    ["<Up>"] = {},
-                    ["<Down>"] = {},
-                    ["<Left>"] = {},
-                    ["<Right>"] = {},
-                },
-            })
-        end,
-    },
-
-    -- 使用 curl 运行请求，使用 jq 格式化，并根据您自己的工作流程保存命令
-    -- 这些命令将打开 curl.nvim 选项卡。在左侧缓冲区中，您可以粘贴或写入 curl 命令，然后按 Enter 键，命令将执行，并且输出将在最右侧的缓冲区中显示和格式化。
-    -- 如果您愿意，您可以选择右侧缓冲区中的文本，并使用 jq 对其进行过滤，即 ggVG! jq '{query goes here}'
-    {
-        "oysandvik94/curl.nvim",
-        cmd = { "CurlOpen" },
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-        config = true,
-    },
-
-    -- 跟踪在 Neovim 中编码所花费的时间
-    -- {
-    -- 	"ptdewey/pendulum-nvim",
-    -- 	config = function()
-    -- 		require("pendulum").setup({
-    -- 			log_file = vim.fn.expand("$HOME/Documents/my_custom_log.csv"),
-    -- 			timeout_len = 300, -- 5 minutes
-    -- 			timer_len = 60, -- 1 minute
-    -- 			gen_reports = true, -- Enable report generation (requires Go)
-    -- 			top_n = 10, -- Include top 10 entries in the report
-    -- 		})
-    -- 	end,
-    -- },
+	-- 跟踪在 Neovim 中编码所花费的时间
+	-- {
+	-- 	"ptdewey/pendulum-nvim",
+	-- 	config = function()
+	-- 		require("pendulum").setup({
+	-- 			log_file = vim.fn.expand("$HOME/Documents/my_custom_log.csv"),
+	-- 			timeout_len = 300, -- 5 minutes
+	-- 			timer_len = 60, -- 1 minute
+	-- 			gen_reports = true, -- Enable report generation (requires Go)
+	-- 			top_n = 10, -- Include top 10 entries in the report
+	-- 		})
+	-- 	end,
+	-- },
 }
 
 return M
---
+++ lua/components/todo.lua
@@ -0,0 +1,60 @@
+local M = {}
+
+M.config = function()
+    local make_pattern_in_comment = function(pattern)
+        return function(buf_id)
+            local cs = vim.bo[buf_id].commentstring
+            if cs == nil or cs == "" then
+                cs = "# %s"
+            end
+
+            -- Extract left and right part relative to '%s'
+            local left, right = cs:match("^(.*)%%s(.-)$")
+            left, right = vim.trim(left), vim.trim(right)
+            -- General ideas:
+            -- - Line is commented if it has structure
+            -- "whitespace - comment left - anything - comment right - whitespace"
+            -- - Highlight pattern only if it is to the right of left comment part
+            --   (possibly after some whitespace)
+            -- Example output for '/* %s */' commentstring: '^%s*/%*%s*()TODO().*%*/%s*'
+            return string.format("^%%s*%s%%s*()%s().*%s%%s*$", vim.pesc(left), pattern, vim.pesc(right))
+        end
+    end
+
+    -- 创建高亮组
+    vim.api.nvim_set_hl(0, "HG_TODO_LIST_WARN", { bold = true, bg = "#ffc777", fg = "#222436" })
+    vim.api.nvim_set_hl(0, "HG_TODO_LIST_FIX", { bold = true, bg = "#c53b53", fg = "#c8d3f5" })
+    vim.api.nvim_set_hl(0, "HG_TODO_LIST_NOTE", { bold = true, bg = "#4fd6be", fg = "#222436" })
+    vim.api.nvim_set_hl(0, "HG_TODO_LIST_TODO", { bold = true, bg = "#0db9d7", fg = "#222436" })
+
+    Create_cmd("TODO", function()
+        require("fzf-lua").grep({ search = "TODO:", no_esc = true })
+    end, { nargs = "*" })
+
+    Create_cmd("WARN", function()
+        require("fzf-lua").grep({ search = "TODO:", no_esc = true })
+    end, { nargs = "*" })
+
+    Create_cmd("FIX", function()
+        require("fzf-lua").grep({ search = "FIX:", no_esc = true })
+    end, { nargs = "*" })
+
+    Create_cmd("NOTE", function()
+        require("fzf-lua").grep({ search = "NOTE:", no_esc = true })
+    end, { nargs = "*" })
+
+    local hipatterns = require("mini.hipatterns")
+    hipatterns.setup({
+        highlighters = {
+            fix = { pattern = make_pattern_in_comment("FIX:"), group = "HG_TODO_LIST_FIX" },
+            warn = { pattern = make_pattern_in_comment("WARN:"), group = "HG_TODO_LIST_WARN" },
+            todo = { pattern = make_pattern_in_comment("TODO:"), group = "HG_TODO_LIST_TODO" },
+            note = { pattern = make_pattern_in_comment("NOTE:"), group = "HG_TODO_LIST_NOTE" },
+
+            -- Highlight hex color strings (`#rrggbb`) using that color
+            hex_color = hipatterns.gen_highlighter.hex_color(),
+        },
+    })
+end
+
+return M
