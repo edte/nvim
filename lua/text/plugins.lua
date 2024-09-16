aac356d96fc0883f0bc4b9c63f0d1896e47fe4ab
orzorzzhang <orzorzzhang@tencent.com>
2024-09-17 05:57:46 +0800

    modify config

--- lua/text/plugins.lua
+++ lua/text/plugins.lua
@@ -1,134 +1,125 @@
 local M = {}
 
 M.list = {
-	-- Treesitter
-	{
-		"nvim-treesitter/nvim-treesitter",
-		cmd = {
-			"TSInstall",
-			"TSUninstall",
-			"TSUpdate",
-			"TSUpdateSync",
-			"TSInstallInfo",
-			"TSInstallSync",
-			"TSInstallFromGrammar",
-			"TSBufToggle",
-		},
-		event = "User FileOpened",
-		config = function()
-			local r = try_require("text.treesitter")
-			if r ~= nil then
-				r.config()
-			end
-		end,
-	},
+    -- Treesitter
+    {
+        "nvim-treesitter/nvim-treesitter",
+        cmd = {
+            "TSInstall",
+            "TSUninstall",
+            "TSUpdate",
+            "TSUpdateSync",
+            "TSInstallInfo",
+            "TSInstallSync",
+            "TSInstallFromGrammar",
+            "TSBufToggle",
+        },
+        event = "User FileOpened",
+        config = function()
+            local r = try_require("text.treesitter")
+            if r ~= nil then
+                r.config()
+            end
+        end,
+    },
 
-	-- 语法感知文本对象、选择、移动、交换和查看支持。
-	{
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		-- keys = { "[m", "]m", "[[", "]]", "[]", "][" },
-	},
+    -- 语法感知文本对象、选择、移动、交换和查看支持。
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        -- keys = { "[m", "]m", "[[", "]]", "[]", "][" },
+    },
 
-	{
-		"David-Kunz/treesitter-unit",
-		event = { "User FileOpened" },
-		after = "nvim-treesitter",
-	},
-	{
-		"JoosepAlviste/nvim-ts-context-commentstring",
-		lazy = true,
-	},
+    {
+        "David-Kunz/treesitter-unit",
+        event = { "User FileOpened" },
+        after = "nvim-treesitter",
+    },
+    {
+        "JoosepAlviste/nvim-ts-context-commentstring",
+        lazy = true,
+    },
 
-	-- 显示代码上下文,包含函数签名
-	-- 只能从下面固定多少个，而不是从上面固定，所以如果套太多层，函数名会显示不出来
-	{
-		"nvim-treesitter/nvim-treesitter-context",
-		after = "nvim-treesitter",
-		event = "BufRead",
-	},
+    -- 显示代码上下文,包含函数签名
+    -- 只能从下面固定多少个，而不是从上面固定，所以如果套太多层，函数名会显示不出来
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        after = "nvim-treesitter",
+        event = "BufRead",
+    },
 
-	-- -- 使用treesitter自动关闭并自动重命名html标签
-	{
-		"windwp/nvim-ts-autotag",
-		ft = { "html", "vue" },
-		config = function()
-			try_require("nvim-ts-autotag").setup()
-		end,
-	},
+    -- -- 使用treesitter自动关闭并自动重命名html标签
+    {
+        "windwp/nvim-ts-autotag",
+        ft = { "html", "vue" },
+        config = function()
+            try_require("nvim-ts-autotag").setup()
+        end,
+    },
 
-	-- Neovim Lua 插件用于拆分和连接参数。 “mini.nvim” 库的一部分。
-	{
-		"echasnovski/mini.splitjoin",
-		init = function()
-			keymap("n", "K", "<cmd>lua require('mini.splitjoin').toggle()<CR>")
-		end,
-		cmd = { "K" },
-		version = false,
-		config = function()
-			require("mini.splitjoin").setup({
-				mappings = {
-					toggle = "K",
-				},
-			})
-		end,
-	},
+    -- Neovim Lua 插件用于拆分和连接参数。 “mini.nvim” 库的一部分。
+    {
+        "echasnovski/mini.splitjoin",
+        init = function()
+            keymap("n", "K", "<cmd>lua require('mini.splitjoin').toggle()<CR>")
+        end,
+        cmd = { "K" },
+        version = false,
+        config = function()
+            require("mini.splitjoin").setup({
+                mappings = {
+                    toggle = "K",
+                },
+            })
+        end,
+    },
 
-	-- 更好的注释生成器。支持多种语言和注释约定。
-	{
-		"danymat/neogen",
-		after = "nvim-treesitter",
-		keys = "gc",
-		config = function()
-			require("neogen").setup({})
-			keymap("n", "gc", "<cmd>lua require('neogen').generate()<CR>")
-		end,
-	},
+    -- 更好的注释生成器。支持多种语言和注释约定。
+    {
+        "danymat/neogen",
+        after = "nvim-treesitter",
+        keys = "gc",
+        config = function()
+            require("neogen").setup({})
+            keymap("n", "gc", "<cmd>lua require('neogen').generate()<CR>")
+        end,
+    },
 
-	-- Telescope
-	{
-		"nvim-telescope/telescope.nvim",
-		config = function()
-			require("text.telescope").config()
-		end,
-		lazy = true,
-		cmd = "Telescope",
-	},
 
-	{
-		"ibhagwan/fzf-lua",
-		cmd = "FzfLua",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("fzf-lua").setup({
-				"telescope",
-				fzf_opts = { ["--cycle"] = "" },
-				winopts = {
-					fullscreen = true,
-				},
-			})
-		end,
-	},
+    {
+        "ibhagwan/fzf-lua",
+        cmd = "FzfLua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("fzf-lua").setup({
+                "telescope",
+                fzf_opts = { ["--cycle"] = "" },
+                winopts = {
+                    fullscreen = true,
+                },
+            })
+        end,
+    },
 
-	-- require("fzf-lua-lazy").search()
-	{
-		url = "https://github.com/roginfarrer/fzf-lua-lazy.nvim.git",
-		config = function()
-			Command("Plugin", function()
-				require("fzf-lua-lazy").search()
-			end, { nargs = "*" })
-		end,
-		cmd = "Plugin",
-	},
+    -- require("fzf-lua-lazy").search()
+    {
+        url = "https://github.com/roginfarrer/fzf-lua-lazy.nvim.git",
+        config = function()
+            Command("Plugin", function()
+                require("fzf-lua-lazy").search()
+            end, { nargs = "*" })
+        end,
+        cmd = "Plugin",
+    },
 
-	-- 如果打开的文件很大，此插件会自动禁用某些功能。文件大小和要禁用的功能是可配置的。
-	-- {
-	-- 	"lunarvim/bigfile.nvim",
-	-- 	config = function()
-	-- 		require("text.bigfile").config()
-	-- 	end,
-	-- 	dependencies = { "nvim-treesitter/nvim-treesitter" },
-	-- 	event = { "FileReadPre", "BufReadPre", "User FileOpened" },
-	-- },
+    -- 如果打开的文件很大，此插件会自动禁用某些功能。文件大小和要禁用的功能是可配置的。
+    -- {
+    -- 	"lunarvim/bigfile.nvim",
+    -- 	config = function()
+    -- 		require("text.bigfile").config()
+    -- 	end,
+    -- 	dependencies = { "nvim-treesitter/nvim-treesitter" },
+    -- 	event = { "FileReadPre", "BufReadPre", "User FileOpened" },
+    -- },
 }
 
 return M
